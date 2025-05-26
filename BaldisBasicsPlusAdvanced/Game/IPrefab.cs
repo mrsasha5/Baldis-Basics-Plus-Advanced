ddffd2c5b2b8220a1dd1b9982ec0a415800dd287
Mr. Sasha <117123038+mrsasha5@users.noreply.github.com>
2025-05-27 02:39:41 +0300

    Delete BaldisBasicsPlusAdvanced directory

--- BaldisBasicsPlusAdvanced/Game/IPrefab.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BaldisBasicsPlusAdvanced.Game
-{
-    public interface IPrefab
-    {
-
-        void InitializePrefab(int variant);
-
-    }
-}
--- BaldisBasicsPlusAdvanced/Game/Interfaces/IRepairable.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BaldisBasicsPlusAdvanced.Game.Interfaces
-{
-    public interface IRepairable
-    {
-
-        void Repair();
-
-    }
-}
