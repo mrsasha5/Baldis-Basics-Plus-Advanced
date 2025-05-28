5971325462e04fece9b5b6f81b7a171a84e2f62f
Mr. Sasha <117123038+mrsasha5@users.noreply.github.com>
2025-05-29 02:33:57 +0300

    v0.2.3.7

---
+++ BaldisBasicsPlusAdvanced/Game/Components/UI/Menu/NotifiedExtraMenu.cs
@@ -0,0 +1,26 @@
+﻿using BaldisBasicsPlusAdvanced.SaveSystem;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BaldisBasicsPlusAdvanced.Game.Components.UI.Menu
+{
+    public class NotifiedExtraMenu : MonoBehaviour
+    {
+        public Image notifImage;
+
+        private void OnDisable()
+        {
+            if (notifImage != null)
+            {
+                Destroy(notifImage.gameObject);
+                Destroy(this);
+                OptionsDataManager.ExtraSettings.showNotif = false; //invokes first than preservation
+            }
+        }
+
+
+    }
+}
---
+++ BaldisBasicsPlusAdvanced/Game/IVariablePrefab.cs
@@ -0,0 +1,9 @@
+﻿/*namespace BaldisBasicsPlusAdvanced.Game
+{
+    interface IVariablePrefab
+    {
+        //public string[] PrefabNames => new string[0];
+
+        public bool SetPrefabProperties(int variant);
+    }
+}*/
